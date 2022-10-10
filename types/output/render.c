hyprwm/wlroots-hyprland

commit 3be6658ee7b6446e438e8d8c6c7bca3c0c0d4bc8

output: allocate swapchain on first commit
On first commit, require a new buffer if the compositor called a
mode-setting function, even if the mode won't change. This makes it
so the swapchain is created now.

Stop trying to check whether the backend supports buffer-less modesets
because that makes everything more complicated. For instance, the
DRM backend doesn't need a new buffer if the previous DRM master left
the output enabled.

Closes: https://gitlab.freedesktop.org/wlroots/wlroots/-/issues/3499
Closes: https://gitlab.freedesktop.org/wlroots/wlroots/-/issues/3502

types/output/render.c
@@ -209,13 +209,13 @@ bool output_ensure_buffer(struct wlr_output *output,
 	if (state->committed & WLR_OUTPUT_STATE_RENDER_FORMAT) {
 		needs_new_buffer = true;
 	}
-	if (!needs_new_buffer) {
-		return true;
+	if (state->allow_artifacts && output->commit_seq == 0) {
+		// On first commit, require a new buffer if the compositor called a
+		// mode-setting function, even if the mode won't change. This makes it
+		// so the swapchain is created now.
+		needs_new_buffer = true;
 	}
-
-	// If the backend doesn't necessarily need a new buffer on modeset, don't
-	// bother allocating one.
-	if (!output->impl->test || output->impl->test(output, state)) {
+	if (!needs_new_buffer) {
 		return true;
 	}
 

commit 650f955507a95c5af2fda1628eafc28ef6d99543

output: fix crash in output_test_with_back_buffer()
The output->impl->test check has been removed, but
output_test_with_back_buffer() hasn't been updated accordingly.

Closes: https://gitlab.freedesktop.org/wlroots/wlroots/-/issues/3506
Fixes: 3be6658ee7b6 ("output: allocate swapchain on first commit")

types/output/render.c
@@ -166,7 +166,9 @@ static bool output_attach_empty_back_buffer(struct wlr_output *output,
 
 static bool output_test_with_back_buffer(struct wlr_output *output,
 		const struct wlr_output_state *state) {
-	assert(output->impl->test != NULL);
+	if (output->impl->test == NULL) {
+		return true;
+	}
 
 	// Create a shallow copy of the state with the empty back buffer included
 	// to pass to the backend.