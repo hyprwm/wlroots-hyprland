hyprwm/wlroots-hyprland

commit 6936e163b514fd4f43a9319f53090527fc411156

backend/drm: short-circuit no-op commits
Some output commits (changing e.g. the output scale or transform)
don't require any change in the KMS state. Instead of going through
a KMS commit, return early. Blocking KMS commits can be expensive.

backend/drm/drm.c
@@ -31,12 +31,15 @@
 #include "render/wlr_renderer.h"
 #include "util/signal.h"
 
-static const uint32_t SUPPORTED_OUTPUT_STATE =
-	WLR_OUTPUT_STATE_BACKEND_OPTIONAL |
+// Output state which needs a KMS commit to be applied
+static const uint32_t COMMIT_OUTPUT_STATE =
 	WLR_OUTPUT_STATE_BUFFER |
 	WLR_OUTPUT_STATE_MODE |
 	WLR_OUTPUT_STATE_ENABLED |
 	WLR_OUTPUT_STATE_GAMMA_LUT;
+
+static const uint32_t SUPPORTED_OUTPUT_STATE =
+	WLR_OUTPUT_STATE_BACKEND_OPTIONAL | COMMIT_OUTPUT_STATE;
 
 bool check_drm_features(struct wlr_drm_backend *drm) {
 	if (drmGetCap(drm->fd, DRM_CAP_CURSOR_WIDTH, &drm->cursor_width)) {
@@ -474,6 +477,11 @@ static bool drm_connector_test(struct wlr_output *output,
 		return false;
 	}
 
+	if ((state->committed & ~COMMIT_OUTPUT_STATE) == 0) {
+		// This commit doesn't change the KMS state
+		return true;
+	}
+
 	if ((state->committed & WLR_OUTPUT_STATE_ENABLED) && state->enabled) {
 		if (output->current_mode == NULL &&
 				!(state->committed & WLR_OUTPUT_STATE_MODE)) {
@@ -558,6 +566,11 @@ bool drm_connector_commit_state(struct wlr_drm_connector *conn,
 
 	if (!drm->session->active) {
 		return false;
+	}
+
+	if ((base->committed & ~COMMIT_OUTPUT_STATE) == 0) {
+		// This commit doesn't change the KMS state
+		return true;
 	}
 
 	struct wlr_drm_connector_state pending = {0};

commit 98a83ce14c96f4727791e9a63fd606fdcaa008fc

backend/drm: fix EINVAL atomic commits after VT switch
The drm_connector_commit_state() call in handle_session_active()
was not resulting in any atomic commit, because it didn't match any
of the if branches: active = true, no new buffer was committed,
and adaptive sync/gamma LUT were unchanged. Thus the commit was a
no-op.

Later on, when the compositor performs regular page-flips, the
kernel would return EINVAL indicating that a modeset was needed.

Rework the logic to use a non-blocking page-flip commit if a buffer
was committed, and use a blocking commit if the connector is on or
is being disabled. The only case where we should skip the atomic
commit is when disabling (active = false) an already-disabled
connector (conn->crtc == NULL).

Note, 6936e163b514 ("backend/drm: short-circuit no-op commits")
has introduced early returns for other situations where we don't
need to perform an atomic commit (e.g. updating scale or transform
of an output).

Fixes: f216e979836a ("backend/drm: drop drm_connector_set_mode()")
Closes: https://gitlab.freedesktop.org/wlroots/wlroots/-/issues/3432

backend/drm/drm.c
@@ -592,18 +592,11 @@ bool drm_connector_commit_state(struct wlr_drm_connector *conn,
 		}
 	}
 
-	if (!pending.active) {
-		if (conn->crtc != NULL && !drm_crtc_commit(conn, &pending, 0, false)) {
-			return false;
-		}
-	} else if (pending.base->committed & WLR_OUTPUT_STATE_BUFFER) {
+	if (pending.base->committed & WLR_OUTPUT_STATE_BUFFER) {
 		if (!drm_crtc_page_flip(conn, &pending)) {
 			return false;
 		}
-	} else if (pending.base->committed & (WLR_OUTPUT_STATE_ADAPTIVE_SYNC_ENABLED |
-			WLR_OUTPUT_STATE_GAMMA_LUT)) {
-		assert(conn->crtc != NULL);
-		// TODO: maybe request a page-flip event here?
+	} else if (pending.active || conn->crtc != NULL) {
 		if (!drm_crtc_commit(conn, &pending, 0, false)) {
 			return false;
 		}