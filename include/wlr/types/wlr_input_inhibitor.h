8cfd44980baae8fdc3eb64461378cadd3362878b
Kirill Primak <vyivel@eclair.cafe>
2022-11-11 21:46:50 +0300

    input-inhibitor: deprecate
    The protocol itself has been deprecated.

--- include/wlr/types/wlr_input_inhibitor.h
+++ include/wlr/types/wlr_input_inhibitor.h
@@ -10,6 +10,11 @@
 #define WLR_TYPES_INPUT_INHIBITOR_H
 #include <wayland-server-core.h>
 
+/*
+ * NOTE: Following the protocol deprecation, wlr/types/wlr_input_inhibitor.h is
+ * deprecated and will be removed in the next release.
+ */
+
 struct wlr_input_inhibit_manager {
 	struct wl_global *global;
 	struct wl_client *active_client;
