hyprwm/wlroots-hyprland

commit 65836ce357e4140d4e5250228a05ddd96119a883

backend/drm: log modesetting commits

backend/drm/drm.c
@@ -581,6 +581,12 @@ bool drm_connector_commit_state(struct wlr_drm_connector *conn,
 		flags |= DRM_MODE_PAGE_FLIP_EVENT;
 	}
 
+	if (pending.modeset) {
+		wlr_drm_conn_log(conn, WLR_INFO, "Modesetting with %dx%d @ %.3f Hz",
+			pending.mode.hdisplay, pending.mode.vdisplay,
+			(float)calculate_refresh_rate(&pending.mode) / 1000);
+	}
+
 	if (!drm_crtc_commit(conn, &pending, flags, false)) {
 		return false;
 	}

commit a40ba16a6414f72ae0224164bb08cd8479dc52ee

backend/drm: fix FPE when disabling output
Fixes: 65836ce357e4 ("backend/drm: log modesetting commits")
Closes: https://gitlab.freedesktop.org/wlroots/wlroots/-/issues/3534

backend/drm/drm.c
@@ -582,9 +582,13 @@ bool drm_connector_commit_state(struct wlr_drm_connector *conn,
 	}
 
 	if (pending.modeset) {
-		wlr_drm_conn_log(conn, WLR_INFO, "Modesetting with %dx%d @ %.3f Hz",
-			pending.mode.hdisplay, pending.mode.vdisplay,
-			(float)calculate_refresh_rate(&pending.mode) / 1000);
+		if (pending.active) {
+			wlr_drm_conn_log(conn, WLR_INFO, "Modesetting with %dx%d @ %.3f Hz",
+				pending.mode.hdisplay, pending.mode.vdisplay,
+				(float)calculate_refresh_rate(&pending.mode) / 1000);
+		} else {
+			wlr_drm_conn_log(conn, WLR_INFO, "Turning off");
+		}
 	}
 
 	if (!drm_crtc_commit(conn, &pending, flags, false)) {