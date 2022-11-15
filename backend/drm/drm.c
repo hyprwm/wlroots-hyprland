hyprwm/wlroots-hyprland

commit 84e727daae6788f9eaf2a2f6e920ce7d40f66aa8

backend/drm: request page-flip event on modeset
The old drm_connector_set_mode() function did that by calling
drm_crtc_page_flip(). We lost this in the refactoring.

Fixes: f216e979836a ("backend/drm: drop drm_connector_set_mode()")

backend/drm/drm.c
@@ -577,6 +577,9 @@ bool drm_connector_commit_state(struct wlr_drm_connector *conn,
 			return false;
 		}
 	}
+	if (pending.modeset) {
+		flags |= DRM_MODE_PAGE_FLIP_EVENT;
+	}
 
 	if (!drm_crtc_commit(conn, &pending, flags, false)) {
 		return false;

commit f863b93c05a73097ccddce1ed066f3f11bfb1c3e

backend/drm: only request page-flip if active
It doesn't make sense to request a page-flip for a disabled output.

Fixes: 84e727daae67 ("backend/drm: request page-flip event on modeset")
Closes: https://gitlab.freedesktop.org/wlroots/wlroots/-/issues/3528

backend/drm/drm.c
@@ -577,7 +577,7 @@ bool drm_connector_commit_state(struct wlr_drm_connector *conn,
 			return false;
 		}
 	}
-	if (pending.modeset) {
+	if (pending.modeset && pending.active) {
 		flags |= DRM_MODE_PAGE_FLIP_EVENT;
 	}
 