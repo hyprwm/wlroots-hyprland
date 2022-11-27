fe21fc6920cc272292a77b46adc79e5db1e21abc
Alexander Orzechowski <alex@ozal.ski>
2022-11-26 20:59:44 -0500

    wlr_scene: Don't cull the entire output when entering direct scanout
    We don't need to.

--- types/scene/wlr_scene.c
+++ types/scene/wlr_scene.c
@@ -1504,8 +1504,10 @@ bool wlr_scene_output_commit(struct wlr_scene_output *scene_output) {
 		scene_output->prev_scanout = scanout;
 		wlr_log(WLR_DEBUG, "Direct scan-out %s",
 			scanout ? "enabled" : "disabled");
-		// When exiting direct scan-out, damage everything
-		wlr_damage_ring_add_whole(&scene_output->damage_ring);
+		if (!scanout) {
+			// When exiting direct scan-out, damage everything
+			wlr_damage_ring_add_whole(&scene_output->damage_ring);
+		}
 	}
 
 	if (scanout) {
