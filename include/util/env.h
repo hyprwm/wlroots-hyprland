dc09585d14b53f186d4be78126824faf27da55b8
Simon Ser <contact@emersion.fr>
2022-12-06 11:42:03 +0100

    util/env: add docs
    I always forget what env_parse_switch() does on error.

--- include/util/env.h
+++ include/util/env.h
@@ -4,8 +4,20 @@
 #include <stdbool.h>
 #include <unistd.h>
 
+/**
+ * Parse a bool from an environment variable.
+ *
+ * On success, the parsed value is returned. On error, false is returned.
+ */
 bool env_parse_bool(const char *option);
 
+/**
+ * Pick a choice from an environment variable.
+ *
+ * On success, the choice index is returned. On error, zero is returned.
+ *
+ * switches is a NULL-terminated array.
+ */
 ssize_t env_parse_switch(const char *option, const char **switches);
 
 #endif
