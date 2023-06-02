hyprwm/wlroots-hyprland

commit 9c9e3f626350fa67405317d60b5dfdb828486b63

cursor: ignore wlr_cursor_set_surface() with same surface
Avoids damaging the output and rendering the cursor again if it
hasn't changed.

types/wlr_cursor.c
@@ -462,6 +462,10 @@ void wlr_cursor_set_surface(struct wlr_cursor *cur, struct wlr_surface *surface,
 
 	struct wlr_cursor_output_cursor *output_cursor;
 	wl_list_for_each(output_cursor, &cur->state->output_cursors, link) {
+		if (surface == output_cursor->surface) {
+			continue;
+		}
+
 		cursor_output_cursor_reset_image(output_cursor);
 
 		output_cursor->surface = surface;

commit 09c87cec3f0ea779717e38b3b886ef9a93840339

cursor: fix wl_pointer.set_cursor hotspot updates
A client might update the cursor with the same wl_surface as
before, but with a different hotspot. Don't ignore such updates.

Fixes: 9c9e3f626350 ("cursor: ignore wlr_cursor_set_surface() with same surface")

types/wlr_cursor.c
@@ -463,7 +463,9 @@ void wlr_cursor_set_surface(struct wlr_cursor *cur, struct wlr_surface *surface,
 
 	struct wlr_cursor_output_cursor *output_cursor;
 	wl_list_for_each(output_cursor, &cur->state->output_cursors, link) {
-		if (surface == output_cursor->surface) {
+		if (surface == output_cursor->surface &&
+				hotspot_x == output_cursor->surface_hotspot.x &&
+				hotspot_y == output_cursor->surface_hotspot.y) {
 			continue;
 		}
 