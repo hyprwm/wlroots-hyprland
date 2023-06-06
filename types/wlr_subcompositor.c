hyprwm/wlroots-hyprland

commit 49e9be62ae92382eddca600082785489c82ea487

subcompositor: consider mapping on parent map
Fixes: https://gitlab.freedesktop.org/wlroots/wlroots/-/issues/3654

types/wlr_subcompositor.c
@@ -189,7 +189,7 @@ const struct wlr_surface_role subsurface_role;
  * - The subsurface has a buffer
  * - Its parent is mapped
  */
-static void subsurface_consider_map(struct wlr_subsurface *subsurface) {
+void subsurface_consider_map(struct wlr_subsurface *subsurface) {
 	if (subsurface->surface->mapped || !wlr_surface_has_buffer(subsurface->surface)) {
 		return;
 	}
@@ -329,7 +329,6 @@ void subsurface_handle_parent_commit(struct wlr_subsurface *subsurface) {
 		subsurface->added = true;
 		wl_signal_emit_mutable(&subsurface->parent->events.new_subsurface,
 			subsurface);
-		subsurface_consider_map(subsurface);
 	}
 }
 

include/types/wlr_subcompositor.h
@@ -3,6 +3,7 @@
 
 #include <wlr/types/wlr_subcompositor.h>
 
+void subsurface_consider_map(struct wlr_subsurface *subsurface);
 void subsurface_handle_parent_commit(struct wlr_subsurface *subsurface);
 
 #endif

types/wlr_compositor.c
@@ -735,6 +735,15 @@ void wlr_surface_map(struct wlr_surface *surface) {
 	}
 	assert(wlr_surface_has_buffer(surface));
 	surface->mapped = true;
+
+	struct wlr_subsurface *subsurface;
+	wl_list_for_each(subsurface, &surface->current.subsurfaces_below, current.link) {
+		subsurface_consider_map(subsurface);
+	}
+	wl_list_for_each(subsurface, &surface->current.subsurfaces_above, current.link) {
+		subsurface_consider_map(subsurface);
+	}
+
 	wl_signal_emit_mutable(&surface->events.map, NULL);
 }
 

commit 25a62a3775932188d7f59077c70b59478cdae184

subcompositor: simplify subsurface_consider_map(), call on add
Fixes: 49e9be62ae92382eddca600082785489c82ea487

types/wlr_subcompositor.c
@@ -183,33 +183,9 @@ static const struct wl_subsurface_interface subsurface_implementation = {
 
 const struct wlr_surface_role subsurface_role;
 
-/**
- * Checks if this subsurface needs to be marked as mapped. The subsurface
- * is considered mapped if both:
- * - The subsurface has a buffer
- * - Its parent is mapped
- */
 void subsurface_consider_map(struct wlr_subsurface *subsurface) {
-	if (subsurface->surface->mapped || !wlr_surface_has_buffer(subsurface->surface)) {
-		return;
-	}
-
-	if (!subsurface->parent->mapped) {
-		return;
-	}
-
-	// Now we can map the subsurface
-	wlr_surface_map(subsurface->surface);
-
-	// Try mapping all children too
-	struct wlr_subsurface *child;
-	wl_list_for_each(child, &subsurface->surface->current.subsurfaces_below,
-			current.link) {
-		subsurface_consider_map(child);
-	}
-	wl_list_for_each(child, &subsurface->surface->current.subsurfaces_above,
-			current.link) {
-		subsurface_consider_map(child);
+	if (subsurface->parent->mapped && wlr_surface_has_buffer(subsurface->surface)) {
+		wlr_surface_map(subsurface->surface);
 	}
 }
 
@@ -312,6 +288,7 @@ void subsurface_handle_parent_commit(struct wlr_subsurface *subsurface) {
 		subsurface->added = true;
 		wl_signal_emit_mutable(&subsurface->parent->events.new_subsurface,
 			subsurface);
+		subsurface_consider_map(subsurface);
 	}
 }
 