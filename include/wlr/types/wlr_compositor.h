hyprwm/wlroots-hyprland

commit 099b9de752f9cc212140533a8a2e20b31aa9028f

compositor: drop role object NULL checks in handlers
Instead, move the check to the caller.

types/data_device/wlr_drag.c
@@ -360,9 +360,6 @@ static void drag_handle_drag_source_destroy(struct wl_listener *listener,
 static void drag_icon_surface_role_commit(struct wlr_surface *surface) {
 	assert(surface->role == &drag_icon_surface_role);
 	struct wlr_drag_icon *icon = surface->role_data;
-	if (icon == NULL) {
-		return;
-	}
 
 	drag_icon_set_mapped(icon, wlr_surface_has_buffer(surface));
 }
@@ -370,9 +367,7 @@ static void drag_icon_surface_role_commit(struct wlr_surface *surface) {
 static void drag_icon_surface_role_destroy(struct wlr_surface *surface) {
 	assert(surface->role == &drag_icon_surface_role);
 	struct wlr_drag_icon *icon = surface->role_data;
-	if (icon == NULL) {
-		return;
-	}
+
 	drag_icon_set_mapped(icon, false);
 	wl_signal_emit_mutable(&icon->events.destroy, icon);
 	free(icon);

types/wlr_compositor.c
@@ -425,7 +425,7 @@ static void surface_commit_state(struct wlr_surface *surface,
 		struct wlr_surface_state *next) {
 	assert(next->cached_state_locks == 0);
 
-	if (surface->role && surface->role->precommit) {
+	if (surface->role_data != NULL && surface->role->precommit != NULL) {
 		surface->role->precommit(surface, next);
 	}
 
@@ -484,7 +484,7 @@ static void surface_commit_state(struct wlr_surface *surface,
 		surface->pending.seq++;
 	}
 
-	if (surface->role && surface->role->commit) {
+	if (surface->role_data != NULL && surface->role->commit != NULL) {
 		surface->role->commit(surface);
 	}
 

types/wlr_input_method_v2.c
@@ -149,9 +149,7 @@ static void popup_surface_set_mapped(
 
 static void popup_surface_surface_role_commit(struct wlr_surface *surface) {
 	struct wlr_input_popup_surface_v2 *popup_surface = surface->role_data;
-	if (popup_surface == NULL) {
-		return;
-	}
+
 	popup_surface_set_mapped(popup_surface, wlr_surface_has_buffer(surface)
 		&& popup_surface->input_method->client_active);
 }
@@ -159,9 +157,7 @@ static void popup_surface_surface_role_commit(struct wlr_surface *surface) {
 static void popup_surface_surface_role_precommit(struct wlr_surface *surface,
 		const struct wlr_surface_state *state) {
 	struct wlr_input_popup_surface_v2 *popup_surface = surface->role_data;
-	if (popup_surface == NULL) {
-		return;
-	}
+
 	if (state->committed & WLR_SURFACE_STATE_BUFFER && state->buffer == NULL) {
 		// This is a NULL commit
 		popup_surface_set_mapped(popup_surface, false);
@@ -170,9 +166,7 @@ static void popup_surface_surface_role_precommit(struct wlr_surface *surface,
 
 static void popup_surface_surface_role_destroy(struct wlr_surface *surface) {
 	struct wlr_input_popup_surface_v2 *popup_surface = surface->role_data;
-	if (popup_surface == NULL) {
-		return;
-	}
+
 	popup_surface_set_mapped(popup_surface, false);
 	wl_signal_emit_mutable(&popup_surface->events.destroy, NULL);
 	wl_list_remove(&popup_surface->link);

types/wlr_layer_shell_v1.c
@@ -315,9 +315,6 @@ void wlr_layer_surface_v1_destroy(struct wlr_layer_surface_v1 *surface) {
 static void layer_surface_role_commit(struct wlr_surface *wlr_surface) {
 	struct wlr_layer_surface_v1 *surface =
 		wlr_layer_surface_v1_from_wlr_surface(wlr_surface);
-	if (surface == NULL) {
-		return;
-	}
 
 	const uint32_t horiz = ZWLR_LAYER_SURFACE_V1_ANCHOR_LEFT |
 		ZWLR_LAYER_SURFACE_V1_ANCHOR_RIGHT;
@@ -370,9 +367,6 @@ static void layer_surface_role_precommit(struct wlr_surface *wlr_surface,
 		const struct wlr_surface_state *state) {
 	struct wlr_layer_surface_v1 *surface =
 		wlr_layer_surface_v1_from_wlr_surface(wlr_surface);
-	if (surface == NULL) {
-		return;
-	}
 
 	if (state->committed & WLR_SURFACE_STATE_BUFFER && state->buffer == NULL) {
 		// This is a NULL commit
@@ -385,9 +379,6 @@ static void layer_surface_role_precommit(struct wlr_surface *wlr_surface,
 static void layer_surface_role_destroy(struct wlr_surface *wlr_surface) {
 	struct wlr_layer_surface_v1 *surface =
 		wlr_layer_surface_v1_from_wlr_surface(wlr_surface);
-	if (surface == NULL) {
-		return;
-	}
 
 	if (surface->configured && surface->mapped) {
 		layer_surface_unmap(surface);

types/wlr_session_lock_v1.c
@@ -135,9 +135,6 @@ static const struct ext_session_lock_surface_v1_interface lock_surface_implement
 static void lock_surface_role_commit(struct wlr_surface *surface) {
 	struct wlr_session_lock_surface_v1 *lock_surface =
 		wlr_session_lock_surface_v1_from_wlr_surface(surface);
-	if (lock_surface == NULL) {
-		return;
-	}
 
 	if (!lock_surface->configured) {
 		wl_resource_post_error(lock_surface->resource,
@@ -167,9 +164,6 @@ static void lock_surface_role_precommit(struct wlr_surface *surface,
 		const struct wlr_surface_state *state) {
 	struct wlr_session_lock_surface_v1 *lock_surface =
 		wlr_session_lock_surface_v1_from_wlr_surface(surface);
-	if (lock_surface == NULL) {
-		return;
-	}
 
 	if (state->committed & WLR_SURFACE_STATE_BUFFER && state->buffer == NULL) {
 		wl_resource_post_error(lock_surface->resource,
@@ -182,9 +176,6 @@ static void lock_surface_role_precommit(struct wlr_surface *surface,
 static void lock_surface_role_destroy(struct wlr_surface *surface) {
 	struct wlr_session_lock_surface_v1 *lock_surface =
 		wlr_session_lock_surface_v1_from_wlr_surface(surface);
-	if (lock_surface == NULL) {
-		return;
-	}
 
 	wl_signal_emit_mutable(&lock_surface->events.destroy, NULL);
 

types/wlr_subcompositor.c
@@ -244,9 +244,6 @@ static void subsurface_unmap(struct wlr_subsurface *subsurface) {
 static void subsurface_role_commit(struct wlr_surface *surface) {
 	struct wlr_subsurface *subsurface =
 		wlr_subsurface_from_wlr_surface(surface);
-	if (subsurface == NULL) {
-		return;
-	}
 
 	subsurface_consider_map(subsurface, true);
 }
@@ -255,9 +252,6 @@ static void subsurface_role_precommit(struct wlr_surface *surface,
 		const struct wlr_surface_state *state) {
 	struct wlr_subsurface *subsurface =
 		wlr_subsurface_from_wlr_surface(surface);
-	if (subsurface == NULL) {
-		return;
-	}
 
 	if (state->committed & WLR_SURFACE_STATE_BUFFER && state->buffer == NULL) {
 		// This is a NULL commit
@@ -268,9 +262,6 @@ static void subsurface_role_precommit(struct wlr_surface *surface,
 static void subsurface_role_destroy(struct wlr_surface *surface) {
 	struct wlr_subsurface *subsurface =
 		wlr_subsurface_from_wlr_surface(surface);
-	if (subsurface == NULL) {
-		return;
-	}
 
 	if (subsurface->has_cache) {
 		wlr_surface_unlock_cached(subsurface->surface,

types/xdg_shell/wlr_xdg_surface.c
@@ -285,9 +285,6 @@ static void xdg_surface_handle_surface_commit(struct wl_listener *listener,
 void xdg_surface_role_commit(struct wlr_surface *wlr_surface) {
 	struct wlr_xdg_surface *surface =
 		wlr_xdg_surface_from_wlr_surface(wlr_surface);
-	if (surface == NULL) {
-		return;
-	}
 
 	surface->current = surface->pending;
 
@@ -319,9 +316,6 @@ void xdg_surface_role_precommit(struct wlr_surface *wlr_surface,
 		const struct wlr_surface_state *state) {
 	struct wlr_xdg_surface *surface =
 		wlr_xdg_surface_from_wlr_surface(wlr_surface);
-	if (surface == NULL) {
-		return;
-	}
 
 	if (state->committed & WLR_SURFACE_STATE_BUFFER && state->buffer == NULL) {
 		// This is a NULL commit
@@ -334,9 +328,6 @@ void xdg_surface_role_precommit(struct wlr_surface *wlr_surface,
 void xdg_surface_role_destroy(struct wlr_surface *wlr_surface) {
 	struct wlr_xdg_surface *surface =
 		wlr_xdg_surface_from_wlr_surface(wlr_surface);
-	if (surface == NULL) {
-		return;
-	}
 
 	reset_xdg_surface(surface);
 

xwayland/xwm.c
@@ -824,9 +824,6 @@ static void read_surface_property(struct wlr_xwm *xwm,
 static void xwayland_surface_role_commit(struct wlr_surface *wlr_surface) {
 	assert(wlr_surface->role == &xwayland_surface_role);
 	struct wlr_xwayland_surface *surface = wlr_surface->role_data;
-	if (surface == NULL) {
-		return;
-	}
 
 	if (!surface->mapped && wlr_surface_has_buffer(surface->surface)) {
 		surface->mapped = true;
@@ -839,9 +836,6 @@ static void xwayland_surface_role_precommit(struct wlr_surface *wlr_surface,
 		const struct wlr_surface_state *state) {
 	assert(wlr_surface->role == &xwayland_surface_role);
 	struct wlr_xwayland_surface *surface = wlr_surface->role_data;
-	if (surface == NULL) {
-		return;
-	}
 
 	if (state->committed & WLR_SURFACE_STATE_BUFFER && state->buffer == NULL) {
 		// This is a NULL commit
@@ -856,9 +850,6 @@ static void xwayland_surface_role_precommit(struct wlr_surface *wlr_surface,
 static void xwayland_surface_role_destroy(struct wlr_surface *wlr_surface) {
 	assert(wlr_surface->role == &xwayland_surface_role);
 	struct wlr_xwayland_surface *surface = wlr_surface->role_data;
-	if (surface == NULL) {
-		return;
-	}
 
 	if (surface->mapped) {
 		wl_signal_emit_mutable(&surface->events.unmap, surface);

commit 753f3cc4fa2aa1d541be12ccc9f81dc9531e23cf

compositor: add wlr_surface_role.no_object
This commit allows to make a role as not represented by an object,
which fixes calling role commit handlers for roles like cursor
surfaces.

Fixes: 099b9de752f9cc212140533a8a2e20b31aa9028f

include/wlr/types/wlr_compositor.h
@@ -74,7 +74,15 @@ struct wlr_surface_state {
 struct wlr_surface_role {
 	const char *name;
 	/**
+	 * If true, the role isn't represented by any object.
+	 * For example, this applies to cursor surfaces.
+	 */
+	bool no_object;
+	/**
 	 * Called when a new surface state is committed. May be NULL.
+	 *
+	 * If the role is represented by an object, this is only called if
+	 * such object exists.
 	 */
 	void (*commit)(struct wlr_surface *surface);
 	/**
@@ -224,6 +232,10 @@ typedef void (*wlr_surface_iterator_func_t)(struct wlr_surface *surface,
 /**
  * Set the lifetime role for this surface. Returns true on success or false if
  * the role cannot be set.
+ *
+ * If the role is represented by an object, role_data must be non-NULL.
+ * Alternatively, if the role isn't represented by any object, role_data must
+ * be NULL.
  */
 bool wlr_surface_set_role(struct wlr_surface *surface,
 	const struct wlr_surface_role *role, void *role_data,

types/data_device/wlr_drag.c
@@ -365,6 +365,7 @@ static void drag_icon_surface_role_commit(struct wlr_surface *surface) {
 
 const struct wlr_surface_role drag_icon_surface_role = {
 	.name = "wl_data_device-icon",
+	.no_object = true,
 	.commit = drag_icon_surface_role_commit,
 };
 

types/seat/wlr_seat_pointer.c
@@ -79,6 +79,7 @@ static void pointer_cursor_surface_handle_commit(struct wlr_surface *surface) {
 
 static const struct wlr_surface_role pointer_cursor_surface_role = {
 	.name = "wl_pointer-cursor",
+	.no_object = true,
 	.commit = pointer_cursor_surface_handle_commit,
 };
 

types/tablet_v2/wlr_tablet_v2_tool.c
@@ -25,6 +25,7 @@ static void tablet_tool_cursor_surface_handle_commit(struct wlr_surface *surface
 
 static const struct wlr_surface_role tablet_tool_cursor_surface_role = {
 	.name = "wp_tablet_tool-cursor",
+	.no_object = true,
 	.commit = tablet_tool_cursor_surface_handle_commit,
 };
 

types/wlr_compositor.c
@@ -491,7 +491,8 @@ static void surface_commit_state(struct wlr_surface *surface,
 		surface->pending.seq++;
 	}
 
-	if (surface->role_data != NULL && surface->role->commit != NULL) {
+	if (surface->role != NULL && surface->role->commit != NULL &&
+			(surface->role_data != NULL || surface->role->no_object)) {
 		surface->role->commit(surface);
 	}
 
@@ -768,6 +769,7 @@ bool wlr_surface_set_role(struct wlr_surface *surface,
 		const struct wlr_surface_role *role, void *role_data,
 		struct wl_resource *error_resource, uint32_t error_code) {
 	assert(role != NULL);
+	assert((role_data == NULL) == role->no_object);
 
 	if (surface->role != NULL && surface->role != role) {
 		if (error_resource != NULL) {