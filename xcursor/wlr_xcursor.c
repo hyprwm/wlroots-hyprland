hyprwm/wlroots-hyprland

commit dbedcdb418f49090df6e7359e6d3e58c97db1a74

xcursor: add fallbacks for legacy names
Some XCursor themes still use the legacy names instead of the newer
cursor naming spec [1]. Fall back to the legacy name if the standard
one could not be found.

[1]: https://www.freedesktop.org/wiki/Specifications/cursor-spec/

Closes: https://gitlab.freedesktop.org/wlroots/wlroots/-/issues/3746

xcursor/wlr_xcursor.c
@@ -239,7 +239,7 @@ void wlr_xcursor_theme_destroy(struct wlr_xcursor_theme *theme) {
 	free(theme);
 }
 
-struct wlr_xcursor *wlr_xcursor_theme_get_cursor(struct wlr_xcursor_theme *theme,
+static struct wlr_xcursor *xcursor_theme_get_cursor(struct wlr_xcursor_theme *theme,
 		const char *name) {
 	for (unsigned int i = 0; i < theme->cursor_count; i++) {
 		if (strcmp(name, theme->cursors[i]->name) == 0) {
@@ -248,6 +248,47 @@ struct wlr_xcursor *wlr_xcursor_theme_get_cursor(struct wlr_xcursor_theme *theme
 	}
 
 	return NULL;
+}
+
+struct wlr_xcursor *wlr_xcursor_theme_get_cursor(struct wlr_xcursor_theme *theme,
+		const char *name) {
+	struct wlr_xcursor *xcursor = xcursor_theme_get_cursor(theme, name);
+	if (xcursor) {
+		return xcursor;
+	}
+
+	// Try the legacy name as a fallback
+	const char *fallback;
+	if (strcmp(name, "default") == 0) {
+		fallback = "left_ptr";
+	} else if (strcmp(name, "text") == 0) {
+		fallback = "xterm";
+	} else if (strcmp(name, "pointer") == 0) {
+		fallback = "hand1";
+	} else if (strcmp(name, "wait") == 0) {
+		fallback = "watch";
+	} else if (strcmp(name, "all-scroll") == 0) {
+		fallback = "grabbing";
+	} else if (strcmp(name, "sw-resize") == 0) {
+		fallback = "bottom_left_corner";
+	} else if (strcmp(name, "se-resize") == 0) {
+		fallback = "bottom_right_corner";
+	} else if (strcmp(name, "s-resize") == 0) {
+		fallback = "bottom_side";
+	} else if (strcmp(name, "w-resize") == 0) {
+		fallback = "left_side";
+	} else if (strcmp(name, "e-resize") == 0) {
+		fallback = "right_side";
+	} else if (strcmp(name, "nw-resize") == 0) {
+		fallback = "top_left_corner";
+	} else if (strcmp(name, "ne-resize") == 0) {
+		fallback = "top_right_corner";
+	} else if (strcmp(name, "n-resize") == 0) {
+		fallback = "top_side";
+	} else {
+		return NULL;
+	}
+	return xcursor_theme_get_cursor(theme, fallback);
 }
 
 static int xcursor_frame_and_duration(struct wlr_xcursor *cursor,

commit d6998d36ed20a2f67a1714f96fd581d6dc0090a0

xcursor: fix duplicate cursor check check in load_callback()
wlr_xcursor_theme_get_cursor() now does some fallback logic. We
don't want that for checking whether a cursor has already been
loaded.

Fixes: dbedcdb418f4 ("xcursor: add fallbacks for legacy names")
Closes: https://gitlab.freedesktop.org/wlroots/wlroots/-/issues/3783

xcursor/wlr_xcursor.c
@@ -170,10 +170,13 @@ static struct wlr_xcursor *xcursor_create_from_xcursor_images(
 	return cursor;
 }
 
+static struct wlr_xcursor *xcursor_theme_get_cursor(struct wlr_xcursor_theme *theme,
+	const char *name);
+
 static void load_callback(struct xcursor_images *images, void *data) {
 	struct wlr_xcursor_theme *theme = data;
 
-	if (wlr_xcursor_theme_get_cursor(theme, images->name)) {
+	if (xcursor_theme_get_cursor(theme, images->name)) {
 		xcursor_images_destroy(images);
 		return;
 	}