hyprwm/wlroots-hyprland

commit 374daeb2560350cf5075837077a3564dfcf316b6

backend/drm: Ensure a primary fb is available when configuring an output

backend/drm/drm.c
@@ -684,6 +684,12 @@ static bool drm_connector_test(struct wlr_output *output,
 	struct wlr_drm_connector_state pending = {0};
 	drm_connector_state_init(&pending, conn, state);
 
+	if (state->allow_reconfiguration && !pending.primary_fb) {
+		wlr_drm_conn_log(conn, WLR_DEBUG,
+			"No primary frame buffer available for this connector");
+		goto out;
+	}
+
 	if ((state->committed & WLR_OUTPUT_STATE_ADAPTIVE_SYNC_ENABLED) &&
 			state->adaptive_sync_enabled &&
 			!drm_connector_supports_vrr(conn)) {

commit 0e839ab1c28f24e68b0266f2e57e530ac8830fc1

backend/drm: fix primary FB check
When turning off a CRTC, we don't need a buffer.

It doesn't matter whether this is a modeset or not: we always need
a buffer even for regular page-flips as long as a connector is
active.

Fixes: 374daeb25603 ("backend/drm: Ensure a primary fb is available when configuring an output")

backend/drm/drm.c
@@ -682,7 +682,7 @@ static bool drm_connector_test(struct wlr_output *output,
 	struct wlr_drm_connector_state pending = {0};
 	drm_connector_state_init(&pending, conn, state);
 
-	if (state->allow_reconfiguration && !pending.primary_fb) {
+	if (pending.active && !pending.primary_fb) {
 		wlr_drm_conn_log(conn, WLR_DEBUG,
 			"No primary frame buffer available for this connector");
 		goto out;