hyprwm/wlroots-hyprland

commit d7d974ae30d1f609dce92266bfe02affba9a28df

backend/drm: drop VRR debug message
These weren't super helpful and will get in our way for
multi-connector commits.

backend/drm/atomic.c
@@ -385,14 +385,6 @@ static bool atomic_crtc_commit(struct wlr_drm_connector *conn,
 		crtc->own_mode_id = true;
 		commit_blob(drm, &crtc->mode_id, mode_id);
 		commit_blob(drm, &crtc->gamma_lut, gamma_lut);
-
-		if (vrr_enabled != prev_vrr_enabled) {
-			output->adaptive_sync_status = vrr_enabled ?
-				WLR_OUTPUT_ADAPTIVE_SYNC_ENABLED :
-				WLR_OUTPUT_ADAPTIVE_SYNC_DISABLED;
-			wlr_drm_conn_log(conn, WLR_DEBUG, "VRR %s",
-				vrr_enabled ? "enabled" : "disabled");
-		}
 	} else {
 		rollback_blob(drm, &crtc->mode_id, mode_id);
 		rollback_blob(drm, &crtc->gamma_lut, gamma_lut);

backend/drm/libliftoff.c
@@ -481,14 +481,6 @@ out:
 		crtc->own_mode_id = true;
 		commit_blob(drm, &crtc->mode_id, mode_id);
 		commit_blob(drm, &crtc->gamma_lut, gamma_lut);
-
-		if (vrr_enabled != prev_vrr_enabled) {
-			output->adaptive_sync_status = vrr_enabled ?
-				WLR_OUTPUT_ADAPTIVE_SYNC_ENABLED :
-				WLR_OUTPUT_ADAPTIVE_SYNC_DISABLED;
-			wlr_drm_conn_log(conn, WLR_DEBUG, "VRR %s",
-				vrr_enabled ? "enabled" : "disabled");
-		}
 	} else {
 		rollback_blob(drm, &crtc->mode_id, mode_id);
 		rollback_blob(drm, &crtc->gamma_lut, gamma_lut);

commit 71be993982ad0bf2bbb3f8abb0c1acb85bd747cd

backend/drm: set adaptive_sync_status again
Fixes: d7d974ae30d1f609dce92266bfe02affba9a28df

backend/drm/atomic.c
@@ -301,6 +301,9 @@ void drm_atomic_connector_apply_commit(struct wlr_drm_connector_state *state) {
 	commit_blob(drm, &crtc->mode_id, state->mode_id);
 	commit_blob(drm, &crtc->gamma_lut, state->gamma_lut);
 
+	conn->output.adaptive_sync_status = state->vrr_enabled ?
+		WLR_OUTPUT_ADAPTIVE_SYNC_ENABLED : WLR_OUTPUT_ADAPTIVE_SYNC_DISABLED;
+
 	destroy_blob(drm, state->fb_damage_clips);
 }
 