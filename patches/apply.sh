caa3c158f78f76edc64d627ed5f27bd21f9bf5c1
Agent_00Ming <agent00ming9366@gmail.com>
2024-04-19 21:38:59 -0400

    Report on patch application failure and exit script with code 1 	modified:   patches/apply.sh

--- patches/apply.sh
+++ patches/apply.sh
@@ -15,13 +15,17 @@ check_applied () {
 	git apply --check --reverse -q -p1 $PATCH
 }
 
+fail () {
+	echo =======\> \'$PATCH\' was not applied && exit 1
+}
+
 if [[ -n "$PATCHES" ]];
 then
-	# check patch validity and apply, else check if already applied and report on failure
+	# check patch validity and apply, else check if already applied and report and exit on failure
 	echo 'Patches found. Applying...';
 	for PATCH in $PATCHES;
 	do
-		check && apply || check_applied || echo =======\> \'$PATCH\' was not applied;
+		check && apply || check_applied || fail;
 	done
 else
 	echo 'No patches found.'
