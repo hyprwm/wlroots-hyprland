d5c08e43bc19606175325d554e6485f0eb3143d8
Jeremy Huang <jeremychhuang@gmail.com>
2024-04-20 15:28:24 -0700

    fix patch script compatibility

--- patches/apply.sh
+++ patches/apply.sh
@@ -1,31 +1,27 @@
-#!/bin/bash
+#!/bin/sh -eu
 
 # find all patches in patches/
 PATCHES=$(find patches/ -type f -name '*.patch')
 
-check () {
-	git apply --check -q -p1 $PATCH
-}
-
 apply () {
-	git apply -p1 $PATCH
+	git apply $PATCH
 }
 
 check_applied () {
-	git apply --check --reverse -q -p1 $PATCH
+	git apply --check --reverse -q $PATCH
 }
 
 fail () {
 	echo =======\> \'$PATCH\' was not applied && exit 1
 }
 
-if [[ -n "$PATCHES" ]];
+if [ -n "$PATCHES" ];
 then
 	# check patch validity and apply, else check if already applied and report and exit on failure
 	echo 'Patches found. Applying...';
 	for PATCH in $PATCHES;
 	do
-		check && apply || check_applied || fail;
+		apply || check_applied || fail;
 	done
 else
 	echo 'No patches found.'
